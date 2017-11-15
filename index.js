884062ca499ca06c87202df02b91edd779af4dde
tobi-stephen <staspyke@gmail.com>
2017-11-15 21:40:52 +0100

    Reviewed

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var app = express();
 var students = require('./routes/students');
-var db = mongoose.connect('mongodb://localhost:27017/love');
-/*var db = mongoose.connect('mongodb://project:project@ds149495.mlab.com:49495/alcs', {
-	useMongoClient: true
-});*/
+//var db = mongoose.connect('mongodb://localhost:27017/love');
+mongoose.connect('mongodb://project:project@ds149495.mlab.com:49495/alcs',function(err){
+	if(err){
+		console.log('Cannot connect to database')
+	}else{
+		console.log('Connection to database was succesful')
+	}
+});
+//var url = 'mongodb://admin:laplace33@cluster0-shard-00-00-ysau0.mongodb.net:27017,cluster0-shard-00-01-ysau0.mongodb.net:27017,cluster0-shard-00-02-ysau0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+//var db = mongoose.connect(url);
 app.set('port', process.env.PORT || 3000);
 
 // view engine setup
